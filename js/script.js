4afa50376d138dc762eb62a784488bce8cd540c2
Bogdan <burlakabogdan@gmail.com>
2021-12-04 09:05:07 +0200

    первый урок

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-newFunction();
 
-function newFunction() {
-    const isChecked=true,
-    isClose = true;
-    console.log(2+2*2 !=='6');
-    console.log(1+1);
-}
+const numberOfFilms = prompt('Сколько фильмов Вы просмотрели?');
+
+const personlMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false 
+};
+
+const lastSeeFilms1 = prompt('Один из просмотренных фильмов');
+const lastSeeFilmsMark1 = +prompt('НА сколько оцените');
+
+const lastSeeFilms2 = prompt('Один из просмотренных фильмов');
+const lastSeeFilmsMark2 = prompt('НА сколько оцените');
+
+personlMovieDB.movies[lastSeeFilms1] = lastSeeFilms1;
+personlMovieDB.movies = lastSeeFilmsMark1;
+personlMovieDB.movies[lastSeeFilms2] = lastSeeFilms2;
+personlMovieDB.movies = lastSeeFilmsMark2;
+
+
+
+console.log(personlMovieDB);
