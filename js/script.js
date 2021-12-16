4db70948b75c9dc6668983b0a24099187a284f27
Bogdan <burlakabogdan@gmail.com>
2021-12-16 22:24:43 +0200

    после дз

--- js/script.js
+++ js/script.js
@@ -28,27 +28,60 @@ const personlMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < numberOfFilms; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < numberOfFilms; i++) {
 
-    lastSeeFilm = prompt('Один из просмотренных фильмов');
-    lastSeeFilmsMark = +prompt('На сколько оцените');
+        lastSeeFilm = prompt('Один из просмотренных фильмов');
+        lastSeeFilmsMark = +prompt('На сколько оцените');
+    
+        if (lastSeeFilm != null && lastSeeFilm.length > 1 && lastSeeFilm.length < 50) {
+            personlMovieDB.movies[lastSeeFilm] = lastSeeFilmsMark;
+            console.log(personlMovieDB);
+        } else {
+            i--;
+            console.log(personlMovieDB);
+        }
+    }
+    
+}
 
-    if (lastSeeFilm != null && lastSeeFilm.length > 1 && lastSeeFilm.length < 50) {
-        personlMovieDB.movies[lastSeeFilm] = lastSeeFilmsMark;
-        console.log(personlMovieDB);
+
+rememberMyFilms();
+
+function  detectPersonlaLevel() {
+    if (personlMovieDB.count < 10) {
+        console.log('Просмотрно мало фильмов');
+    } else if (personlMovieDB.count > 10 && personlMovieDB.count < 30) {
+        console.log('классический зритель');
+    } else if (personlMovieDB.count > 30) {
+        console.log('киноман');
+    
     } else {
-        i--;
+        console.log('произошла ошибка');
+    }
+}
+
+detectPersonlaLevel();
+
+function showMyDB () {
+    if (personlMovieDB.privat ===false) {
         console.log(personlMovieDB);
     }
 }
 
-if (personlMovieDB.count < 10) {
-    console.log('Просмотрно мало фильмов');
-} else if (personlMovieDB.count > 10 && personlMovieDB.count < 30) {
-    console.log('классический зритель');
-} else if (personlMovieDB.count > 30) {
-    console.log('киноман');
-
-} else {
-    console.log('произошла ошибка');
+function writeYourGenres () {
+    let qw = '';
+    for (let i=0;i<3;i++) {
+        qw = +prompt ('Ваш любимый жанр под номером ${номер по порядку}');
+        while(qw===null || qw ==='' || isNaN(qw)) {
+            qw = +prompt ('Ваш любимый жанр под номером ${номер по порядку}');
+        }
+        personlMovieDB.genres.push(qw);
+    } 
 }
+
+
+showMyDB();
+writeYourGenres();
+
+console.log(personlMovieDB);
