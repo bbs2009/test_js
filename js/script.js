9d73c9719cfe6faf9bdedea829bbbf2e78578b48
Bogdan <burlakabogdan@gmail.com>
2021-12-16 22:00:23 +0200

    2 задание

--- js/script.js
+++ js/script.js
@@ -8,45 +8,47 @@ let lastSeeFilmsMark;
 
 
 
-numberOfFilms = +prompt ('Сколько фильмов просмотрено?','');
-while (numberOfFilms===null) {
-numberOfFilms = +prompt('ВВедите сколько фильмові Ві просмотрели');    
-}
+
 // console.log(numberOfFilms);
 
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов просмотрено?', '');
+    while (numberOfFilms === null || numberOfFilms =='' || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('ВВедите сколько фильмові Ві просмотрели');
+    }
+}
+
+start();
+
 const personlMovieDB = {
-    count : numberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
-    genres:[],
-    privat: false 
+    genres: [],
+    privat: false
 };
 
-for (let i=0; i<numberOfFilms;i++) {
-    
+for (let i = 0; i < numberOfFilms; i++) {
+
     lastSeeFilm = prompt('Один из просмотренных фильмов');
     lastSeeFilmsMark = +prompt('На сколько оцените');
 
-    if (lastSeeFilm != null && lastSeeFilm.length>1 && lastSeeFilm.length <50) {
+    if (lastSeeFilm != null && lastSeeFilm.length > 1 && lastSeeFilm.length < 50) {
         personlMovieDB.movies[lastSeeFilm] = lastSeeFilmsMark;
         console.log(personlMovieDB);
-    }
-    else {
+    } else {
         i--;
         console.log(personlMovieDB);
     }
 }
 
-if (personlMovieDB.count<10) {
+if (personlMovieDB.count < 10) {
     console.log('Просмотрно мало фильмов');
-}
-else if (personlMovieDB.count>10 && personlMovieDB.count<30) {
-    console.log ('классический зритель');
-}
-else if(personlMovieDB.count>30) {
+} else if (personlMovieDB.count > 10 && personlMovieDB.count < 30) {
+    console.log('классический зритель');
+} else if (personlMovieDB.count > 30) {
     console.log('киноман');
 
-}
-else {
+} else {
     console.log('произошла ошибка');
 }
---
+++ js/script2.js
@@ -0,0 +1,20 @@
+
+
+
+const testFunc = function (){
+    const str = 'test';
+    const fruit = 'some fruit';
+    const logg = 'hello world';
+    // console.log(str.toUpperCase());
+    // console.log(fruit.indexOf('q'));
+    // console.log( logg.slice(-2, -1));
+    // console.log(logg.substring(6,11));
+   // console.log(logg.substr(6,2));
+   const num = 12.522;
+   const test = '12.12x';
+   console.log(Math.round(num,3));
+   console.log(parseFloat(test));
+};
+
+
+testFunc();
