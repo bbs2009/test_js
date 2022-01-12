216703314fe5cac616a9e818a120df2973302032
Bogdan <burlakabogdan@gmail.com>
2022-01-12 22:32:15 +0200

    food 1 lesson done

--- .vscode/food/dist/js/script.js
+++ .vscode/food/dist/js/script.js
@@ -3,6 +3,65 @@
 
 document.addEventListener("DOMContentLoaded", ()=>{
     
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    
+    function hideTabContent(){
+
+        tabsContent.forEach(item=>{
+            // item.style.display = 'none';
+            
+            item.classList.add('hide', 'fade');
+            item.classList.remove('show');
+
+        });
+
+        tabs.forEach(item=>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(el=0) {
+        tabsContent.forEach((item, ind)=>{
+           if (el===ind){
+            //    item.style.display='block';
+               item.classList.add('show', 'fade');
+               item.classList.remove('hide');
+
+               tabs[ind].classList.add('tabheader__item_active');
+           }
+        
+
+        });
+    }
+
+    function handlerClick(){
+        tabsParent.addEventListener('click',e=>{
+            if (e.target && e.target.classList.contains('tabheader__item')){
+                // console.log(e.target);
+                tabs.forEach((item, ind)=>{
+                    if (item===e.target){
+                        console.log(item);
+                        console.log(e.target);
+                        
+                        hideTabContent();
+                        showTabContent(ind);
+                    }
+
+                });
+            }
+        });
+    }
+
+    hideTabContent();
+    showTabContent();
+    handlerClick();
+
+
+
+
     
 
 });
